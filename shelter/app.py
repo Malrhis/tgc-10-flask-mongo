78aa4dca4b62b57779ab9495191c38f41199931d
Gabriel Seet <malrhis@gmail.com>
2021-03-14 13:43:12 +0000

    create shelter folder

---
+++ shelter/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+MONGO_URI = os.environ.get('MONGO_URI')
+DB_NAME = 'tgc10_new_shelter'
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
